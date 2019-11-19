2fffd21167249bb02949f0a037288cf61d1d8cc0
dev09 <devdevdevdevdev09@gmail.com>
2019-11-19 22:00:07 +0900

    eamil update

--- tutorial-w3c/day3/email.js
+++ tutorial-w3c/day3/email.js
@@ -1,26 +1,28 @@
 var nodemailer = require('nodemailer');
 
+const USER_NAME    = process.argv[2];
+const USER_PASS    = process.argv[3];
+const TO_USER_NAME = process.argv[4];
+
 var transporter = nodemailer.createTransport({
-    service : 'gmail',
-    auth : {
-        user : 'youremail@gmail.com',
-        pass : 'yourpassword'
-    }
+  service: 'gmail',
+  auth: {
+    user: USER_NAME,
+    pass: USER_PASS
+  }
 });
 
 var mailOptions = {
-    form : 'yourmail@gmail.com',
-    to : 'myfriend@yahoo.com',
-    subject : 'Sending Email using node.js',
-    text : 'That was easy!'
+  from: USER_NAME,
+  to: TO_USER_NAME,
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
 };
 
 transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-    }else{
-        console.log('Email sent : ' + info.response);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
-
-// TODO : 구글 안전하지 않은 앱 열기
